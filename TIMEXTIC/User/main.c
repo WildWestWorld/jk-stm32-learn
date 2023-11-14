a2b7f06b632cd0d9cfc30cd2a3535ffc64aa13b4
ljk <380686356@qq.com>
2023-11-14 22:00:45 +0800

    完成定时器中断

---
+++ TIMEXTIC/User/main.c
@@ -0,0 +1,37 @@
+#include "stm32f10x.h"                  // Device header
+#include "Delay.h"
+#include "OLED.h"
+#include "Timer.h"
+
+uint8_t KeyNum;
+uint16_t Num;
+
+int main(void)
+{
+			OLED_Init();
+			Timer_Init();
+	
+	OLED_ShowString(1,1,"Num:");
+	OLED_ShowString(2,1,"CNT:");
+	while(1){
+		OLED_ShowNum(1,5,Num,5);
+		//显示TIM2的CNT计数器
+		//自增的范围是0- 我们自己填的数 本次填写的是10000  //对应的就是1s
+		OLED_ShowNum(2,5,Timer_GetCounter(),5);
+
+	}
+	
+}
+
+
+void TIM2_IRQHandler(void){
+	//获取中断标志位 
+	if(TIM_GetITStatus(TIM2,TIM_IT_Update)==SET){
+		
+		Num ++;
+		
+		//清楚中断标志位
+		TIM_ClearITPendingBit(TIM2,TIM_IT_Update);
+	}
+	
+}
